d82ee6c02d10d30bbb1eeb2296518acb0bc3b4b4
Jasper Alblas <just_jasper@hotmail.com>
2026-01-03 21:36:25 +0100

    Adds more tests

--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -3,3 +3,11 @@ const capitalize = require("./capitalize");
 test("capital first letter", () => {
   expect(capitalize("awesome")).toBe("Awesome");
 });
+
+test("capital all letters", () => {
+  expect(capitalize("COOL")).toBe("COOL");
+});
+
+test("capital all except", () => {
+  expect(capitalize("fIRE")).toBe("FIRE");
+});
